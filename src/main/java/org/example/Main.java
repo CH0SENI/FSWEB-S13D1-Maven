0c01edd29533bc80990059afa3916e2d69fb1d97
CH0SENI <serdarfirattt@gmail.com>
2025-10-06 17:53:56 +0300

    Maven

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,22 +6,42 @@ public class Main {
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+        if (clock < 0 || clock > 23) {
+            return false; // geçersiz saat..
+        }
+        if (!isBarking) {
+            return false; // köpek havlamıyor
+        }
+        return (clock >= 20 || clock < 8);
     }
 
-    public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+    public static boolean hasTeen(int a, int b, int c) {
+        return isTeen(a) || isTeen(b) || isTeen(c);
+    }
+
+    private static boolean isTeen(int age) {
+        return age >= 13 && age <= 19;
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
+        if (isSummer) {
+            return temp >= 25 && temp <= 45;
+        } else {
+            return temp >= 25 && temp <= 35;
+        }
     }
 
     public static double area(double width, double height) {
-       
+        if (width < 0 || height < 0) {
+            return -1;
+        }
+        return width * height;
     }
 
     public static double area(double radius) {
-       
+        if (radius < 0) {
+            return -1;
+        }
+        return radius * radius * Math.PI;
     }
 }
